94b89d173fa01668cdd2d2e5fc3b9f48e1038f94
Vladas Zakrevskis <146100@gmail.com>
2022-01-17 00:19:37 +0200

    ios tokio

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ use test_engine::{
     ui::{input::touch::Event, Touch},
     Screen,
 };
+use tokio::runtime::Runtime;
 
 use crate::controls_view::ControlsView;
 
@@ -19,43 +20,46 @@ mod controls_view;
 mod game_level;
 
 static mut SCREEN: *mut Screen = ptr::null_mut();
+static mut RUNTIME: Option<Runtime> = None;
 
 #[no_mangle]
-pub extern "C" fn create_screen() {
-    let mut screen = Box::new(Screen::new(Default::default()));
-    screen.ui.set_view(ControlsView::boxed());
-    screen.ui.add_debug_view();
+pub unsafe extern "C" fn create_screen() {
+    RUNTIME = Some(tokio::runtime::Runtime::new().unwrap());
 
-    unsafe {
+    RUNTIME.as_ref().unwrap().block_on(async {
+        let mut screen = Box::new(Screen::new(Default::default()));
+        screen.ui.set_view(ControlsView::boxed());
+        screen.ui.add_debug_view();
+
         SCREEN = Box::into_raw(screen);
-    }
+    });
 }
 
 #[no_mangle]
-pub extern "C" fn set_screen_size(width: c_float, height: c_float) {
-    unsafe {
+pub unsafe extern "C" fn set_screen_size(width: c_float, height: c_float) {
+    RUNTIME.as_ref().unwrap().block_on(async {
         SCREEN
             .as_mut()
             .unwrap_unchecked()
             .set_size(Size { width, height });
-    }
+    });
 }
 
 #[no_mangle]
-pub extern "C" fn update_screen() {
-    unsafe {
+pub unsafe extern "C" fn update_screen() {
+    RUNTIME.as_ref().unwrap().block_on(async {
         SCREEN.as_mut().unwrap_unchecked().update();
-    }
+    });
 }
 
 #[no_mangle]
-pub extern "C" fn on_touch(id: c_ulong, x: c_float, y: c_float, event: c_int) {
+pub unsafe extern "C" fn on_touch(id: c_ulong, x: c_float, y: c_float, event: c_int) {
     #[allow(clippy::useless_conversion)]
-    unsafe {
+    RUNTIME.as_ref().unwrap().block_on(async {
         SCREEN.as_mut().unwrap_unchecked().ui.on_touch(Touch {
             id:       id.into(),
             position: (x * 2.0, y * 2.0).into(),
             event:    Event::from_int(event),
         })
-    }
+    });
 }
