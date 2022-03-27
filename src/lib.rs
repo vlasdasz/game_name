c44637b17a611c29069caba65b7cc2050bdeca84
Vladas Zakrevskis <146100@gmail.com>
2022-03-27 16:33:28 +0300

    fixed ios, common code with te

--- src/lib.rs
+++ src/lib.rs
@@ -9,68 +9,33 @@ use std::{
     ptr,
 };
 
-use test_engine::{
-    gl_wrapper::monitor::Monitor,
-    gm::Size,
-    rtools::Boxed,
-    ui::{input::touch::Event, Touch},
-    Screen,
-};
-use tokio::runtime::Runtime;
+use test_engine::app::App;
 
 use crate::controls_view::ControlsView;
 
 mod controls_view;
 mod game_level;
 
-static mut SCREEN: *mut Screen = ptr::null_mut();
-static mut MONITOR: *mut Monitor = ptr::null_mut();
-
-static mut RUNTIME: Option<Runtime> = None;
+static mut APP: *mut App<ControlsView> = ptr::null_mut();
 
 #[no_mangle]
-pub unsafe extern "C" fn create_screen(width: c_int, height: c_int) {
-    RUNTIME = Some(tokio::runtime::Runtime::new().unwrap());
-
-    RUNTIME.as_ref().unwrap().block_on(async {
-        let mut screen = Box::new(Screen::new((width, height).into()));
-
-        screen.ui.set_view(ControlsView::boxed());
-        screen.ui.add_debug_view();
-
-        screen.add_monitor(MONITOR.as_ref().unwrap().clone());
-
-        SCREEN = Box::into_raw(screen);
-    });
+pub extern "C" fn create_screen(width: c_int, height: c_int) {
+    unsafe { APP.as_mut().unwrap_unchecked().create_screen(width, height) }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn set_screen_size(width: c_float, height: c_float) {
-    RUNTIME.as_ref().unwrap().block_on(async {
-        SCREEN
-            .as_mut()
-            .unwrap_unchecked()
-            .set_size(Size { width, height });
-    });
+pub extern "C" fn set_screen_size(width: c_int, height: c_int) {
+    unsafe { APP.as_mut().unwrap_unchecked().set_screen_size(width, height) }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn update_screen() {
-    RUNTIME.as_ref().unwrap().block_on(async {
-        SCREEN.as_mut().unwrap_unchecked().update();
-    });
+pub extern "C" fn update_screen() {
+    unsafe { APP.as_mut().unwrap_unchecked().update_screen() }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn on_touch(id: c_ulong, x: c_float, y: c_float, event: c_int) {
-    #[allow(clippy::useless_conversion)]
-    RUNTIME.as_ref().unwrap().block_on(async {
-        SCREEN.as_mut().unwrap_unchecked().ui.on_touch(Touch {
-            id:       id.into(),
-            position: (x * 2.0, y * 2.0).into(),
-            event:    Event::from_int(event),
-        })
-    });
+pub extern "C" fn on_touch(id: c_ulong, x: c_float, y: c_float, event: c_int) {
+    unsafe { APP.as_mut().unwrap_unchecked().on_touch(id, x, y, event) }
 }
 
 #[no_mangle]
@@ -84,19 +49,17 @@ pub extern "C" fn set_monitor(
     height: c_float,
     diagonal: c_float,
 ) {
-    let monitor = Monitor::new(
-        "Phone screen".into(),
-        ppi as _,
-        scale,
-        refresh_rate as _,
-        (resolution_x, resolution_y).into(),
-        (width, height).into(),
-        diagonal as _,
-    );
-
-    dbg!(&monitor);
-
     unsafe {
-        MONITOR = Box::into_raw(Box::new(monitor));
+        APP = Box::into_raw(Box::new(App::<ControlsView>::default()));
+        APP.as_mut().unwrap_unchecked().set_monitor(
+            ppi,
+            scale,
+            refresh_rate,
+            resolution_x,
+            resolution_y,
+            width,
+            height,
+            diagonal,
+        );
     }
 }
